b70264360dbad6ff15b0bf344d41ff48d1bf5668
William-MC-Flores <william.m.c.flores@outllok.com.br>
2026-03-23 19:48:54 -0300

    estrutura inicial do projeto

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Sistema de Loja</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<h1>Sistema de Pedidos da Loja</h1>
+
+</body>
+</html>
