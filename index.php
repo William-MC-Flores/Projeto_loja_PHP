f62eaf1380e6f417488df532be3456ea2b45b3b7
William-MC-Flores <william.m.c.flores@outllok.com.br>
2026-03-23 19:52:30 -0300

    criação da classe Cliente com encapsulamento

--- index.php
+++ index.php
@@ -1,13 +1,24 @@
+<?php
+require_once "classes/Cliente.php";
+
+$cliente = new Cliente(1, "João Silva", "joao@email.com");
+
+echo $cliente->getNome();
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <title>Sistema de Loja</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
 
-<h1>Sistema de Pedidos da Loja</h1>
+    <h1>Sistema de Pedidos da Loja</h1>
 
 </body>
+
 </html>
